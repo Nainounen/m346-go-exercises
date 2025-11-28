a93b8229531c1ec9a1452d711f049e8b6325d2d9
Nino Meier <nino.meier@swisscom.com>
2025-11-28 12:28:43 +0100

    go-2-ex-3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	modules := map[uint]string{
+		104: "Ein modul das ich nicht kenne",
+		117: "Ein weiteres Modul das ich nicht kenne",
+		346: "Cloud-Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 104)
+
+	modules[320] = "c#"
+
+	modules[117] = "Ich kenne es doch"
 	fmt.Println(modules)
 }
