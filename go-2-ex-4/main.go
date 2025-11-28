176e35bf3c58aea4c825b7ae798f23dba83e209d
Nino Meier <nino.meier@swisscom.com>
2025-11-28 12:50:21 +0100

    go-2-ex-4

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,34 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+type Class map[byte]Student
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	class1 := Class{
+		1: {FirstName: "Joe", LastName: "Doe"},
+		2: {FirstName: "Jay", LastName: "Day"},
+		3: {FirstName: "Wef", LastName: "EwfwefDay"},
+	}
+
+	class2 := Class{
+		1: {FirstName: "Jim", LastName: "Jam"},
+		2: {FirstName: "Jam", LastName: "Bam"},
+		3: {FirstName: "Jan", LastName: "Beam"},
+	}
+
+	modules := map[uint][]Class{
+		346: {class1, class2},
+		320: {class1},
+		114: {class2},
+	}
+
+	fmt.Println(class1)
+	fmt.Println(class2)
+	fmt.Println(modules)
 }
