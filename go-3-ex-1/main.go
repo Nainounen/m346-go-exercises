0847db67918ab8008fca225475a42a9aa6a4775f
Nino Meier <nino.meier@swisscom.com>
2025-12-05 12:17:54 +0100

    go-3-ex-1

--- go-3-ex-1/main.go
+++ go-3-ex-1/main.go
@@ -20,8 +20,33 @@ const (
 func outputWithZodiacSign(p Person) {
 	var zodiacSign rune = '?'
 
-	// TODO: Assign proper value to zodiacSign using if/else branching.
-	// NOTE: The runes are defined above as constants.
+	//used LLM for this formula
+	zodiacNumber := int(p.Month)*100 + int(p.Day)
+	if zodiacNumber >= 321 && zodiacNumber <= 420 {
+		zodiacSign = Aries
+	} else if zodiacNumber >= 421 && zodiacNumber <= 521 {
+		zodiacSign = Taurus
+	} else if zodiacNumber >= 522 && zodiacNumber <= 621 {
+		zodiacSign = Gemini
+	} else if zodiacNumber >= 622 && zodiacNumber <= 722 {
+		zodiacSign = Cancer
+	} else if zodiacNumber >= 723 && zodiacNumber <= 822 {
+		zodiacSign = Leo
+	} else if zodiacNumber >= 823 && zodiacNumber <= 922 {
+		zodiacSign = Virgo
+	} else if zodiacNumber >= 923 && zodiacNumber <= 1022 {
+		zodiacSign = Libra
+	} else if zodiacNumber >= 1023 && zodiacNumber <= 1122 {
+		zodiacSign = Scorpius
+	} else if zodiacNumber >= 1123 && zodiacNumber <= 1220 {
+		zodiacSign = Sagittarius
+	} else if zodiacNumber >= 1221 || zodiacNumber <= 119 {
+		zodiacSign = Capricornus
+	} else if zodiacNumber >= 120 && zodiacNumber <= 218 {
+		zodiacSign = Aquarius
+	} else if zodiacNumber >= 219 && zodiacNumber <= 320 {
+		zodiacSign = Pisces
+	}
 
 	fmt.Printf("%s %s, born on %02d.%02d.%04d, has the zodiac sign %c.\n",
 		p.FirstName, p.LastName, p.Day, p.Month, p.Year, zodiacSign)
