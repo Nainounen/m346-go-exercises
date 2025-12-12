78d271341ac2eb13796c11b000007f5a0ade6b18
Nino Meier <nino.meier@swisscom.com>
2025-12-12 12:55:42 +0100

    go-4-ex-4

--- go-4-ex-4/main.go
+++ go-4-ex-4/main.go
@@ -1,9 +1,20 @@
 package main
 
-// TODO: implement the function convertCelsiusToFahrenheit
-// TODO: implement the function convertFahrenheitToCelsius
+import "fmt"
 
+func convertFahrenheitToCelsius(F float32) float32 {
+	return (F - 32) * (5.0 / 9.0)
+}
+func convertCelsiusToFahrenheit(C float32) float32 {
+	return C*(9.0/5.0) + 32
+}
 func main() {
-	// TODO: call the function convertCelsiusToFahrenheit
-	// TODO: call the function convertFahrenheitToCelsius
+
+	fmt.Println(convertCelsiusToFahrenheit(29.5)) //85.1
+	fmt.Println(convertCelsiusToFahrenheit(22.1)) //71.78
+	fmt.Println(convertCelsiusToFahrenheit(12.3)) //54.14
+
+	fmt.Println(convertFahrenheitToCelsius(convertCelsiusToFahrenheit(29.5))) //29.5                          //
+	fmt.Println(convertFahrenheitToCelsius(convertCelsiusToFahrenheit(22.1))) //22.1
+	fmt.Println(convertFahrenheitToCelsius(convertCelsiusToFahrenheit(12.3))) //12.3
 }
