98136af4d9f5f7fc9602882cacc4df5d08d4a6a6
Nino Meier <nino.meier@swisscom.com>
2025-12-12 12:22:42 +0100

    go-4-ex-2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,21 @@
 package main
 
-// TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+import (
+	"fmt"
+	"math"
+)
+
+type ShortSides struct {
+	a float64
+	b float64
+}
+
+func computeHypotenuse(s ShortSides) float64 {
+	return math.Sqrt(math.Pow(s.a, 2) + math.Pow(s.b, 2))
+}
 
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Println(computeHypotenuse(ShortSides{2.4, 5.7}))   //6.18465843842649
+	fmt.Println(computeHypotenuse(ShortSides{25.2, 12.6})) //28.17445651649735
+	fmt.Println(computeHypotenuse(ShortSides{21.4, 9.0}))  //23.21551205552012
 }
